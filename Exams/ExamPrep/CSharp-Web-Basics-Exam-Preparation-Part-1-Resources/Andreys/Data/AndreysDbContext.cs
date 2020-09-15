e397901d5d5e8f467f991dee3ad2e47f7afb1f88
GeorgiTsekov <53428844+GeorgiTsekov@users.noreply.github.com>
2020-09-15 14:21:44 +0300

    add register, login, logout

--- Exams/ExamPrep/CSharp-Web-Basics-Exam-Preparation-Part-1-Resources/Andreys/Data/AndreysDbContext.cs
+++ Exams/ExamPrep/CSharp-Web-Basics-Exam-Preparation-Part-1-Resources/Andreys/Data/AndreysDbContext.cs
@@ -1,9 +1,25 @@
 ﻿namespace Andreys.Data
 {
+    using Andreys.Models;
     using Microsoft.EntityFrameworkCore;
 
     public class AndreysDbContext : DbContext
     {
-   
+        public AndreysDbContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseSqlServer(@"Server=DESKTOP-M0LGMHO\SQLEXPRESS;Database=Anderys;Integrated Security=True");
+
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
