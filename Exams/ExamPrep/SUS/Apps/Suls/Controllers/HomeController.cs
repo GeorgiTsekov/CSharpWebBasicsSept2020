bfa3c2cefe68eb11f1cafecc724c2a3145962db9
Georgi Tsekov <georgi_tsekov@abv.bg>
2020-10-20 14:41:55 +0300

    Work on Suls exam

---
+++ Exams/ExamPrep/SUS/Apps/Suls/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Suls.ViewModels.Problems;
+using SUS.HTTP;
+using SUS.MvcFramework;
+using System.Collections.Generic;
+
+namespace Suls.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public HttpResponse Index()
+        {
+            if (this.IsUserSignedIn())
+            {
+                return this.View(new List<HomePageProblemViewModel>(), "IndexLoggedIn");
+            }
+            else
+            {
+                return this.View();
+            }
+        }
+    }
+}
---
+++ Exams/ExamPrep/SUS/Apps/Suls/Data/Submission.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Suls.Data
+{
+    public class Submission
+    {
+        //•	Has Code – a string with min length 30 and max length 800 (required)
+        //•	Has Achieved Result – an integer between 0 and 300 (required)
+        //•	Has a Created On – a DateTime object (required)
+        public Submission()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+
+        [Required]
+        [MaxLength(800)]
+        public string Code { get; set; }
+
+        [Required]
+        public int AchievedResult { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string ProblemId { get; set; }
+        public virtual Problem Problem { get; set; }
+
+        public string UserId { get; set; }
+        public virtual User User { get; set; }
+    }
+}
